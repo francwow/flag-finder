503251b988c8cc1aca5e569417588389507af51b
francwow <francwow06@gmail.com>
2023-01-13 11:19:19 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// flag finder apptree
+
+let artUrl = "https://restcountries.com/v3.1/all";
+let imgContainer = document.querySelector(".container");
+let input = document.getElementById("search");
+let searchBtn = document.getElementById("submit");
+
+async function getJSON() {
+  let getData = await fetch(artUrl);
+  let data = await getData.json();
+  console.log(data);
+  return data;
+}
+
+getJSON()
+  .then((data) => {
+    searchBtn.addEventListener("click", () => {
+      imgContainer.innerHTML = "";
+      // console.log(input.value);
+      data.forEach((element) => {
+        // console.log(element.name.official);
+        if (input.value === element.name.common) {
+          console.log(element.name.official);
+          let title = document.createElement("h2");
+          let flag = document.createElement("img");
+          flag.src = element.flags.png;
+          title.innerHTML = element.name.common;
+          imgContainer.append(title, flag);
+        }
+      });
+    });
+    input.addEventListener("keydown", function (e) {
+      if (e.key === "Enter") {
+        imgContainer.innerHTML = "";
+        // console.log(input.value);
+        data.forEach((element) => {
+          // console.log(element.name.official);
+          if (input.value === element.name.common) {
+            console.log(element.name.official);
+            let title = document.createElement("h2");
+            let flag = document.createElement("img");
+            flag.src = element.flags.png;
+            title.innerHTML = element.name.common;
+            imgContainer.append(title, flag);
+          }
+        });
+      }
+    });
+  })
+  .catch((err) => console.log("errors " + err.message));
